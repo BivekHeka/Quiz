49b77b04d5d8f368b51bbef10a563581a3cc356c
Bivek Heka <bivek@example.com>
2025-11-24 18:00:20 +0545

    first commit

---
+++ data.py
@@ -0,0 +1,42 @@
+question_data=[
+  {
+    "text": "Slug is green in color",
+    "answer": "True"
+  },
+  {
+    "text": "The Earth revolves around the Sun",
+    "answer": "True"
+  },
+  {
+    "text": "A triangle has four sides",
+    "answer": "False"
+  },
+  {
+    "text": "Water freezes at 0 degrees Celsius",
+    "answer": "True"
+  },
+  {
+    "text": "The human body has three lungs",
+    "answer": "False"
+  },
+  {
+    "text": "Mount Everest is the tallest mountain on Earth",
+    "answer": "True"
+  },
+  {
+    "text": "Dogs are herbivores",
+    "answer": "False"
+  },
+  {
+    "text": "The Atlantic Ocean is the largest ocean on Earth",
+    "answer": "False"
+  },
+  {
+    "text": "Lightning is hotter than the surface of the Sun",
+    "answer": "True"
+  },
+  {
+    "text": "Sound travels faster than light",
+    "answer": "False"
+  }
+]
